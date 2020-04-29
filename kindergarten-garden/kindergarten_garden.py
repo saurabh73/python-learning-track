867c2f57aa55603adb90634e71669a515ef2524f
Saurabh Dutta <saurabhdutta.jk@gmail.com>
2020-04-29 02:47:15 +0100

    Format Code

--- kindergarten-garden/kindergarten_garden.py
+++ kindergarten-garden/kindergarten_garden.py
@@ -4,17 +4,17 @@ class Garden:
         self.garden = [list(s) for s in diagram.splitlines()]
         self.students = [student.lower() for student in students]
         self.students.sort()
-        self.plant_values = dict({
-        'G': 'Grass',
-        'C': 'Clover',
-        'R': 'Radishes',
-        'V': 'Violets',
-    })
+        self.plant_values = {
+            'G': 'Grass',
+            'C': 'Clover',
+            'R': 'Radishes',
+            'V': 'Violets',
+        }
 
     def plants(self, student):
         index = self.students.index(student.lower())
         ans = []
-        for i in range(0,2):
-            for j in range(0,2):
+        for i in range(0, 2):
+            for j in range(0, 2):
                 ans.append(self.plant_values[self.garden[i][2*index + j]])
         return ans
